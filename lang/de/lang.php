e446137d5518bf481f293fbdfd8ad3143abad4a0
Danny Lin <danny0838@pchome.com.tw>
2011-01-10 19:48:07 +0800

    Add German Translation

---
+++ lang/de/lang.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * German language file
+ * by vehtoh.de
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+$lang['title']              = '%s Verwaltung';
+$lang['redirecttext']       = 'Diese Seite wurde [[:@ID@|hierhin]] verschoben.';
+
+// rename page = rp
+$lang['rp_title']           = 'Seitenumbenennung';
+$lang['rp_newpage']         = 'neuer Name';
+$lang['rp_summary']         = 'Grund';
+
+$lang['rp_oldsummary']      = '(Löschen) %s umbenannt in %s';
+$lang['rp_oldsummaryx']     = '(Löschen) %s umbenannt in %s (%s)';
+$lang['rp_newsummary']      = '%s umbenannt in %s';
+$lang['rp_newsummaryx']     = '%s umbenann in %s (%s)';
+
+$lang['rp_msg_old_empty']   = 'Sie müssen den alten Seitennamen angeben.';
+$lang['rp_msg_old_noexist'] = 'Die alte seite %s gibt es nicht.';
+$lang['rp_msg_new_empty']   = 'Sie müssen einen neuen Seitennamen angeben.';
+$lang['rp_msg_new_exist']   = 'Die neue Seite %S gibt es schon.';
+$lang['rp_msg_locked']      = 'Die Seite %s ist gesperrt.';
+$lang['rp_msg_auth']        = 'Sie haben nicht die erforderlichen Rechte um die Seite %s zu bearbeiten.';
+$lang['rp_msg_file_conflict'] = 'Es ist ein Konflikt mit der neuen Seite %s aufgetreten.';
+$lang['rp_msg_success']     = 'Die Seite %s wurde in %s umbenannt.';
+
+// delete page = dp
+$lang['dp_title']           = 'Seitenlöschung';
+$lang['dp_purge']           = 'Verlauf aufräumen';
+
+$lang['dp_oldsummary']      = 'gelöscht';
+$lang['dp_oldsummaryx']     = 'gelöscht (%s)';
+
+$lang['dp_msg_old_empty']   = 'Sie müssen den alten Seitennamen angeben.';
+$lang['dp_msg_auth']        = 'Sie haben nicht die erforderlichen Rechte um die Seite %s zu löschen.';
+$lang['dp_msg_auth_new']    = 'Sie haben nicht die erforderlichen Rechte um %s zu speichern.';
+$lang['dp_msg_d_success']   = 'Seite %s wurde gelöscht.';
+$lang['dp_msg_p_success']   = 'Seite %s wurde aufgeräumt.';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/de/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * German language file
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author:     vehtoh <kontakt[at]vehtoh[dot]de>
+ */
+
+$lang['redirecttext']    = 'Nach der Seitenverschiebung blieb noch Text übrig. (leere Seite als Standartseite)';
