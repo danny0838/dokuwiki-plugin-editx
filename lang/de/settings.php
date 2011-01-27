1f075a5ff132d7986c13443bdc548c8c96a54f1f
Danny Lin <danny0838@pchome.com.tw>
2011-01-27 12:29:42 +0800

    Update German translation.

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -6,5 +6,7 @@
  * @author:     vehtoh <kontakt[at]vehtoh[dot]de>
  */
 
-$lang['redirecttext']    = 'Nach der Seitenverschiebung blieb noch Text übrig. (Leere Seite als Standardseite)';
-
+$lang['user_rename']     = 'Gruppen/Benutzer, die Seiten umbennen dürfen (Komma getrennt auflisten). Leer, für alle Benutzer.';
+$lang['user_rename_nr']  = 'Gruppen/Benutzer, die Seiten umbennen dürfen, ohne Berechtigung für eine Weiterleitung (Komma getrennt auflisten). Leer, für alle Benutzer.';
+$lang['user_delete']     = 'Gruppen oder Benutzer, die Seiten löschen dürfen (Komma getrennt auflisten). Leer, für alle Benutzer.';
+$lang['redirecttext']    = 'Text, der nach einer Seitenverschiebung auf dem ursprünglichen Platz angezeigt werden soll. Leer für Standardtext.';
