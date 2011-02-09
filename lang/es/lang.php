8c2733b182cd3acbda19d59b34334a4e3445fab2
Danny Lin <danny0838@pchome.com.tw>
2011-02-09 19:58:41 +0800

    Add Spanish Translation

---
+++ lang/es/lang.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * English language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+$lang['title']              = '%s Manager';
+$lang['redirecttext']       = 'Esta página es redireccionada a [[:@ID@]].';
+
+// rename page = rp
+$lang['rp_title']           = 'Renombrar página';
+$lang['rp_newpage']         = 'Nombre nuevo';
+$lang['rp_summary']         = 'Motivo';
+$lang['rp_nr']              = 'No redireccionar';
+
+$lang['rp_oldsummary']      = '(Borrar) %s renombrar a %s';
+$lang['rp_oldsummaryx']     = '(Borrar) %s renombrar a %s (%s)';
+$lang['rp_newsummary']      = '%s renombrar a %s';
+$lang['rp_newsummaryx']     = '%s renombrar a %s (%s)';
+
+$lang['rp_msg_old_empty']   = 'La página antigüa no puede estar vacía.';
+$lang['rp_msg_old_noexist'] = 'La página antigüa %s no existe.';
+$lang['rp_msg_new_empty']   = 'El nombre de la página no puede estar vacía.';
+$lang['rp_msg_new_exist']   = 'La nueva página %s ya existe.';
+$lang['rp_msg_locked']      = 'La página %s está en estos momentos bloqueada.';
+$lang['rp_msg_auth']        = 'No estas autorizado para editar la página %s.';
+$lang['rp_msg_auth_nr']     = 'No estas autorizado para suprimir el enlace.';
+$lang['rp_msg_file_conflict'] = 'Ha ocurrido un error con el fichero de la página %s.';
+$lang['rp_msg_success']     = 'La página %s se ha renombrado correctamente %s.';
+
+// delete page = dp
+$lang['dp_title']           = 'Borrar página';
+$lang['dp_purge']           = 'No borrar historial';
+$lang['dp_summary']         = 'Motivo';
+
+$lang['dp_oldsummary']      = 'Eliminada';
+$lang['dp_oldsummaryx']     = 'Eliminada (%s)';
+
+$lang['dp_msg_old_empty']   = 'El nombre de la página antigüa no puede estar vacía.';
+$lang['dp_msg_auth']        = 'No estas autorizado para eliminar páginas %s.';
+$lang['dp_msg_auth_new']    = 'No estas autorizado para escribir %s.';
+$lang['dp_msg_success']     = 'Página %s eliminada correctamente.';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/es/settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * English language file
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author:     Danny Lin <danny0838[at]pchome[dot]com[dot]tw>
+ */
+
+$lang['user_rename']     = 'Grupo o usuarios que pueden renombrar una página (comma seperated). Dejar en blanco para permitir todos.';
+$lang['user_rename_nr']  = 'Grupos o usuario que pueden renombrar la página sin crear enlaces de redireccionamientos (separados por comas). Dejar en blanco para permitir todos.';
+$lang['user_delete']     = 'Grupos o usuarios que pueden eliminar una página (separados por comas). Dejar en blanco para permitir todos.';
+$lang['redirecttext']    = 'Dejar el texto a la izquierda después de mover la página (dejar en blanco para poner el texto por defecto)';
