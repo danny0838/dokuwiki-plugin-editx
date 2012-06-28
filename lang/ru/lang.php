45659dd58c48f5fd004141a11e2c2f4d278fda1b
hatifnatt <na@na.local>
2012-06-28 18:56:18 +0200

    added russian language files

---
+++ lang/ru/lang.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Russian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+$lang['title']              = 'Управление %s';
+$lang['redirecttext']       = 'Данная страница перемещена сюда [[:@ID@]].';
+
+// rename page = rp
+$lang['rp_title']           = 'Переименовать страницу';
+$lang['rp_newpage']         = 'Новое имя';
+$lang['rp_summary']         = 'Причина';
+$lang['rp_nr']              = 'Не устанавливать переадресацию';
+
+$lang['rp_oldsummary']      = '(Удалить) %s переименовано в %s';
+$lang['rp_oldsummaryx']     = '(Удалить) %s переименовано в %s (%s)';
+$lang['rp_newsummary']      = '%s переименовано в %s';
+$lang['rp_newsummaryx']     = '%s переименовано в %s (%s)';
+
+$lang['rp_msg_old_empty']   = 'Имя старой страницы не может быть пустым.';
+$lang['rp_msg_old_noexist'] = 'Старая страница %s не существует.';
+$lang['rp_msg_new_empty']   = 'Имя новой страницы не может быть пустым.';
+$lang['rp_msg_new_exist']   = 'Страница с именем %s уже существует.';
+$lang['rp_msg_locked']      = 'Страница %s сейчас заблокированна.';
+$lang['rp_msg_auth']        = 'У вас нет прав на редактирование данной страницы %s.';
+$lang['rp_msg_auth_nr']     = 'У вас нет прав на отключение переадресации.';
+$lang['rp_msg_file_conflict'] = 'Конфликт с файлом новой страницы %s.';
+$lang['rp_msg_success']     = 'Страница %s успешно переименована в %s.';
+
+// delete page = dp
+$lang['dp_title']           = 'Удалить страницу';
+$lang['dp_purge']           = 'Не удалять историю';
+$lang['dp_summary']         = 'Причина';
+
+$lang['dp_oldsummary']      = 'Удалено';
+$lang['dp_oldsummaryx']     = 'Удалено (%s)';
+
+$lang['dp_msg_old_empty']   = 'Имя старой страницы не может быть пустым.';
+$lang['dp_msg_auth']        = 'У вас нет прав на удаление данной страницы %s.';
+$lang['dp_msg_auth_new']    = 'У вас нет прав на запись %s.';
+$lang['dp_msg_success']     = 'Страница %s успешно удалена.';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
