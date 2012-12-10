e27feb61eddf5ca1956e72c26410eac30cbc47f4
Jorne Kandziora <jorne@quarantainenet.nl>
2012-12-10 12:05:58 +0100

    Bugfix: Added missing requires to the plugin code

--- action.php
+++ action.php
@@ -6,6 +6,9 @@
 
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
+require_once (DOKU_PLUGIN . 'action.php');
+require_once(DOKU_INC.'inc/fulltext.php');
 
 class action_plugin_editx extends DokuWiki_Action_Plugin {
 
