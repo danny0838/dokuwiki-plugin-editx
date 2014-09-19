e4776950f5e79e9b385bb911aceea3ede11f67b7
Turki <dturkalj@users.noreply.github.com>
2014-09-19 15:44:24 +0200

    Croatian translate

---
+++ lang/hr/lang.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Croatian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+$lang['title']              = '%s upravljanje';
+$lang['redirecttext']       = 'Ova stranica je preusmjerena na [[:@ID@]].';
+$lang['pagemanagement']     = 'upravljanje stranicom';
+
+// rename page = rp
+$lang['rp_title']           = 'Preimenovanje stranice';
+$lang['rp_newpage']         = 'Novo ime';
+$lang['rp_summary']         = 'Razlog';
+$lang['rp_nr']              = 'Ne ostavi preusmjerenje';
+$lang['rp_confirm']         = 'Potvrdi preimenovanje stranice';
+
+$lang['rp_oldsummary']      = '(Obriši) %s preimenovan u %s';
+$lang['rp_oldsummaryx']     = '(Obriši) %s preimenovan u %s (%s)';
+$lang['rp_newsummary']      = '%s preimenovan u %s';
+$lang['rp_newsummaryx']     = '%s preimenovan u %s (%s)';
+
+$lang['rp_msg_unconfirmed'] = 'Uključi kvačicu potvrde da bi preimenovali stranicu.';
+$lang['rp_msg_old_empty']   = 'Staro ime stranice nemože biti prazno.';
+$lang['rp_msg_old_noexist'] = 'Stara stranica %s ne postoji.';
+$lang['rp_msg_new_empty']   = 'Novo ime stranice nemože biti prazno.';
+$lang['rp_msg_new_exist']   = 'Nova stranica %s već postoji.';
+$lang['rp_msg_locked']      = 'Stranica %s je trenutno zaključana.';
+$lang['rp_msg_auth']        = 'Niste autorizirani da uređujete stranicu %s.';
+$lang['rp_msg_auth_nr']     = 'Niste autorizirani da You are not authorized to suzbijete redirekciju.';
+$lang['rp_msg_file_conflict'] = 'Nova stranica %s ima konfliktnu datoteku %s.';
+$lang['rp_msg_success']     = 'Stranica %s uspješno preimenovana u %s.';
+
+// delete page = dp
+$lang['dp_title']           = 'Brisanje stranice';
+$lang['dp_purge']           = 'Ne stvaraj povijest brisanja';
+$lang['dp_confirm']         = 'Potvrdi brisanje stranice';
+$lang['dp_summary']         = 'Razlog';
+
+$lang['dp_oldsummary']      = 'Obrisano';
+$lang['dp_oldsummaryx']     = 'Obrisano (%s)';
+
+$lang['dp_msg_unconfirmed'] = 'Uključi kvačicu potvrde da bi obrisali stranicu.';
+$lang['dp_msg_old_empty']   = 'Staro ime stranice nemože biti prazno.';
+$lang['dp_msg_auth']        = 'Niste autorizirani da obrišete stranicu %s.';
+$lang['dp_msg_auth_new']    = 'Niste autorizirani da pišete u %s.';
+$lang['dp_msg_success']     = 'Stranica %s uspješno obrisana.';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
